359e5c50be62c440aab05c18edebc0c5e16bd4cd
pepa65 <solusos@passchier.net>
2017-01-31 19:46:46 +0700

    Sanitize names Make the feature-specification more human friendly Make all function variables local Fix errortrap to work properly checkcaddy: body in a function, like getcaddy Use the proper download links for this branch

--- checkcaddy.sh
+++ checkcaddy.sh
@@ -1,50 +1,69 @@
 #!/bin/bash
 
-# caddy-upgrade-check.sh
+# checkcaddy.sh
 #
 # Checking the Caddy web server download page for the current version and
 # calling the upgrade script 'getcaddy.sh' on a new version.
 #
-# Usage: caddy-upgrade-check.sh [caddy_install_location]
+# Usage: checkcaddy.sh [caddy_cmd]  # where caddy_cmd is the install location
 
-# Locations of the to-be-upgraded caddy binary and upgrade script
-caddy_path=
-getcaddy=/usr/local/bin/getcaddy.sh
+checkcaddy(){
+	set -E
+	trap 'echo "Aborted, error $? in command: $BASH_COMMAND"; return 1' ERR
 
-# Accommodate sourcing and executing
-[[ $- = *i* ]] && Exit(){ return $1;} || Exit(){ exit $1;}
+	# Locations of the to-be-upgraded caddy binary and upgrade script
+	local caddy_cmd=""
+	local getcaddy_cmd=/usr/local/bin/getcaddy.sh
 
-# Use caddy_install_location commandline option if present
-[[ $1 ]] && caddy_path=$1
-
-# If not hard-coded then find in PATH
-if [[ -z $caddy_path ]]
-then
-	uname="$(uname)"
-	declare -u unameu=$uname
-	[[ $unameu = *WIN* ]] && caddy_bin=caddy.exe || caddy_bin=caddy
-	caddy_path=$(type -p "$caddy_bin")
-fi
-[[ ! -x $caddy_path ]] \
-		&& echo "Aborted $0, no caddy binary at $caddy_path" \
-		&& Exit 1
-
-# Get version from installed binary
-version=$("$caddy_path" -version || true)
-[[ -z $version ]] \
-		&& echo "Aborted $0, caddy binary $caddy_path doesn't work" \
-		&& Exit 2
-
-web_version=$(wget -qO- caddyserver.com/download |grep -o 'Version [.0-9]*'
-
-if [[ ${version##* } != ${web_version##* } ]]
-then
-	if [[ ! -f "$getcaddy" ]]
+	# Use caddy_install_location commandline option if present
+	if [[ $1 ]]
 	then
-		((EUID)) && [[ -z "$ANDROID_ROOT" ]] && sudo_cmd="sudo"
-		$sudo_cmd wget -qO /usr/local/bin/getcaddy.sh loof.bid/getcaddy
+		[[ ${1:0:1} = / ]] && caddy_cmd=$1 || caddy_cmd=$PWD/$1
 	fi
-	bash "$getcaddy" , "$caddy_path"
-fi
 
-Exit 0
+	# If not hard-coded then find in PATH
+	if [[ -z $caddy_cmd ]]
+	then
+		local uname="$(uname)"
+		local -u unameu=$uname
+		local caddy_bin
+		[[ $unameu = *WIN* ]] && caddy_bin=caddy.exe || caddy_bin=caddy
+		caddy_cmd=$(type -p "$caddy_bin")
+	fi
+	[[ ! -x $caddy_cmd ]] \
+			&& echo "Aborted, no caddy binary at $caddy_cmd" \
+			&& return 1
+
+	# Get version from installed binary
+	local version=$("$caddy_cmd" -version)
+	[[ -z $version ]] \
+			&& echo "Aborted, caddy binary $caddy_cmd doesn't work" \
+			&& return 2
+
+	local web_version=$(wget -qO- caddyserver.com/download |grep -o 'Version [^<]*')
+	[[ -z $web_version ]] \
+			&& echo "Aborted, version not found in http://caddyserver.com/download" \
+			&& return 3
+	if [[ ${version##* } != ${web_version##* } ]]
+	then  # different version: upgrade
+		if [[ ! -f $getcaddy_cmd ]]
+		then  # no upgrade file at default location
+			if local getcaddy_type=$(type -p getcaddy.sh)
+			then  # no upgrade file found in path
+				getcaddy_cmd=$getcaddy_type
+			else
+				local sudo_cmd
+				((EUID)) && [[ -z "$ANDROID_ROOT" ]] && sudo_cmd="sudo"
+				$sudo_cmd wget -qO "$getcaddy_cmd" loof.bid/getcaddy
+			fi
+		fi
+		bash "$getcaddy_cmd" , "$caddy_cmd"
+	fi
+
+	trap ERR
+	return 0
+}
+
+checkcaddy "$@"
+checkcaddy_return=$?
+((checkcaddy_return)) && echo "Not completed, aborted at $checkcaddy_return"
--- getcaddy.sh
+++ getcaddy.sh
@@ -2,46 +2,43 @@
 #
 #   Caddy web server installer and upgrade script
 #
-#   Homepage: https://caddyserver.com
-#   Issues:   https://github.com/caddyserver/getcaddy.com/issues
-#   Requires: bash, coreutils, sed, grep, curl / wget, tar / unzip
+#   Caddy homepage: https://caddyserver.com
+#   Getcaddy issues: https://github.com/pepa65/getcaddy.com/issues
+#   Required: bash, coreutils, sed, grep, curl/wget, tar (or unzip on OSX and Windows)
 #
 # Bash script to install the single-binary Caddy web server. Use it like this:
 #
-#   $ curl https://getcaddy.com |bash
+#   $ curl -sL loof.bid/getcaddy |bash
 #	or
-#	  $ wget -qO- https://getcaddy.com |bash
-#
-# In automated environments, you may want to run as root.
-# If using curl, we recommend using the -fsSL flags.
+#	  $ wget -qO- loof.bid/getcaddy |bash
 #
 # If you want to get Caddy with extra features, use -s with a
 # comma-separated list of directives, like this:
 #
-#	  $ curl https://getcaddy.com |bash -s git,mailout
+#	  $ wget -qO- loof.bid/getcaddy |bash -s git,mailout
 #
 # Or it can be first downloaded and then run:
 #
-#   $ wget getcaddy.com -O getcaddy.sh
+#   $ wget -O getcaddy.sh loof.bid/getcaddy
 #   $ bash getcaddy.sh git,mailout
 #
 # See https://caddyserver.com/download for the full list of available features.
 #
 # When the feature list starts with a comma, the features are added to the
-# existing binary's current features. A sole comma means: keep the same features.
+# existing binary's current features. When the feature list is 'all', all
+# available features will be added in. Just 'same' means: keep the same
+# features. And 'none' means: no features will be included.
 #
 # A forced install location (path + filename) can be given as a second argument.
-# If no features are wanted, 'HTTP' can be specified as the feature list.
-#
-#   $ bash getcaddy.sh HTTP /root/caddyserver
 #
 # This should all work on Mac, Linux, and BSD systems, and
 # hopefully Windows with Cygwin. Please open an issue if you notice any bugs.
 #
 
-install_caddy()
+getcaddy()
 {
-	trap 'echo -e "Aborted, error $? in command: $BASH_COMMAND"; trap ERR; return 1' ERR
+	set -E
+	trap 'echo -e "Aborted, error $? in command: $BASH_COMMAND"; return 1' ERR
 
 	#########################
 	# Which OS and version? #
@@ -50,62 +47,54 @@ install_caddy()
 	# Determine arch (and arm)
 	# NOTE: `uname -m` is more accurate and universal than `arch`
 	# See https://en.wikipedia.org/wiki/Uname
-	caddy_arm=""
-	unamem="$(uname -m)"
-	if [[ $unamem == *aarch64* ]]; then
-		caddy_arch="arm64"
-	elif [[ $unamem == *64* ]]; then
-		caddy_arch="amd64"
-	elif [[ $unamem == *86* ]]; then
-		caddy_arch="386"
-	elif [[ $unamem == *armv5* ]]; then
-		caddy_arch="arm"
-		caddy_arm="5"
-	elif [[ $unamem == *armv6l* ]]; then
-		caddy_arch="arm"
-		caddy_arm="6"
-	elif [[ $unamem == *armv7l* ]]; then
-		caddy_arch="arm"
-		caddy_arm="7"
-	else
-		echo "Aborted, unsupported or unknown architecture: $unamem"
-		return 2
-	fi
+	local caddy_arm=""
+	local caddy_arch
+	local unamem="$(uname -m)"
+	case $unamem in
+		*aarch64*) caddy_arch="arm64" ;;
+		*64*) caddy_arch="amd64" ;;
+		*86*) caddy_arch="386" ;;
+		*armv5*) caddy_arch="arm" caddy_arm="5" ;;
+		*armv6l*) caddy_arch="arm" caddy_arm="6" ;;
+		*armv7l*) caddy_arch="arm" caddy_arm="7" ;;
+		*) echo "Aborted, unsupported or unknown architecture: $unamem"
+			return 2 ;;
+	esac
 
 	# Determine os, binary name and package type
-	caddy_bin="caddy"
-	caddy_dl_ext=".tar.gz"
-	uname="$(uname)"
-	declare -u unameu=$uname
-	if [[ ${unameu} == *DARWIN* ]]; then
+	local caddy_os
+	local caddy_dl_ext=".tar.gz"
+	local caddy_bin="caddy"
+	local uname="$(uname)"
+	local -u unameu=$uname
+	if [[ ${unameu} == *DARWIN* ]]
+	then
 		caddy_os="darwin"
 		caddy_dl_ext=".zip"
-		vers=$(sw_vers)
-		version=${vers##*ProductVersion: }
+		local vers=$(sw_vers)
+		local version=${vers##*ProductVersion: }
+		local OSX_MAJOR OSX_MINOR
 		IFS='.' read OSX_MAJOR OSX_MINOR _ <<<"$version"
-
-		# Major
-		if ((OSX_MAJOR < 10)); then
-			echo "Aborted, unsupported OS X version (9-)"
-			return 3
-		fi
-		if ((OSX_MAJOR > 10)); then
-			echo "Aborted, unsupported OS X version (11+)"
-			return 4
-		fi
-
-		# Minor
-		if ((OSX_MINOR < 5)); then
-			echo "Aborted, unsupported OS X version (10.5-)"
-			return 5
-		fi
-	elif [[ ${unameu} == *LINUX* ]]; then
+		((OSX_MAJOR < 10)) \
+			 && echo "Aborted, unsupported OS X version (9-)" \
+			 && return 3
+		((OSX_MAJOR > 10)) \
+				&& echo "Aborted, unsupported OS X version (11+)" \
+				&& return 4
+		((OSX_MINOR < 5)) \
+				&& echo "Aborted, unsupported OS X version (10.5-)" \
+				&& return 5
+	elif [[ ${unameu} == *LINUX* ]]
+	then
 		caddy_os="linux"
-	elif [[ ${unameu} == *FREEBSD* ]]; then
+	elif [[ ${unameu} == *FREEBSD* ]]
+	then
 		caddy_os="freebsd"
-	elif [[ ${unameu} == *OPENBSD* ]]; then
+	elif [[ ${unameu} == *OPENBSD* ]]
+	then
 		caddy_os="openbsd"
-	elif [[ ${unameu} == *WIN* ]]; then
+	elif [[ ${unameu} == *WIN* ]]
+	then
 		# Should catch cygwin
 		caddy_os="windows"
 		caddy_dl_ext=".zip"
@@ -115,19 +104,22 @@ install_caddy()
 		return 6
 	fi
 
-	# determine install location, caddy_path is the full path
-	if [[ $2 ]]; then  # if specified: force that install location
-		caddy_path="$2"
-		[[ ${caddy_path:0:1} = / ]] || caddy_path="./$caddy_path"
-	elif pid=$(pgrep -nx "$caddy_bin"); then  # most recent match if running
+	# determine install location
+	local caddy_cmd pid bin
+	if [[ $2 ]]
+	then  # if specified: force that install location
+		caddy_cmd="$2"
+		[[ ${caddy_cmd:0:1} = / ]] || caddy_cmd="$PWD/$caddy_cmd"
+	elif pid=$(pgrep -nx "$caddy_bin")
+	then  # most recent match if running
 		bin=$(ls -l /proc/$pid/exe)  # if running: use location of the binary
-		caddy_path=$(sed "s@^.* /proc/$pid/exe -> @@" <<<"$bin")
+		caddy_cmd=$(sed "s@^.* /proc/$pid/exe -> @@" <<<"$bin")
 	else
-		caddy_path=$(type -p "$caddy_bin" || true)  # use the first caddy binary in PATH
+		caddy_cmd=$(type -p "$caddy_bin" || true)  # first caddy binary in PATH
 	fi
 
 	# find the curl or wget binaries
-	! dl_cmd="$(type -p curl) -fsSL" && !dl_cmd="$(type -p wget) -qO-" \
+	! local dl_cmd="$(type -p curl) -fsSL" && ! dl_cmd="$(type -p wget) -qO-" \
 			&& echo "Aborted, could not find curl or wget" && return 7
 
 	###################
@@ -135,63 +127,67 @@ install_caddy()
 	###################
 
 	# determine valid features
-	valid_features=$($dl_cmd caddyserver.com/features.json \
+	local valid_features=$($dl_cmd caddyserver.com/features.json \
 			|sed 's/},/\0\n/g' |grep -o '^.*","name":"[^"]*' |grep -o '[^"]*$')
-	valid_features=",$(sed 's/ /,/g' <<<$valid_features),"
+	valid_features=$(sed 's/ /,/g' <<<$valid_features)
 
-	features=$1
+	local features=$1
+	[[ $features = all ]] && features=$valid_features
+	[[ $features = none ]] && features=""
+	[[ $features = same ]] && features=","
 	# if features starts with comma: get the current plugins
-	if [[ ${features:0:1} = ',' ]]; then
+	if [[ ${features:0:1} = "," ]]
+	then
 		# if no binary found, just install a new one with the listed features
-		if [[ -x $caddy_path ]]; then  # read plugins from binary if present
-			plugins=$("$caddy_path" -plugins |grep ' http\.' |sed 's/^.*http\.//')
+		if [[ -x $caddy_cmd ]]
+		then  # read plugins from binary if present
+			local plugins=$("$caddy_cmd" -plugins |grep ' http\.' |sed 's/^.*http\.//' )
 			# only add valid features in the plugins list
 			shopt -s nullglob
+			local plugin
 			for plugin in ${plugins[@]}; do
-				[[ $valid_features = *,$plugin,* ]] && features+=",$plugin"
+				[[ ",$valid_features," = *,$plugin,* ]] && features+=",$plugin"
 			done
 		fi
 	fi
 
 	# validate the specified features
-	featurelist=($(sed 's/,/ /g' <<<"$features"))
+	local featurelist=($(sed 's/,/ /g' <<<"$features"))
+	local feature
 	for feature in "${featurelist[@]}"; do
-		if [[ $valid_features != *,$feature,* ]]; then
+		if [[ ",$valid_features," != *,$feature,* ]]
+		then
 			echo "Aborted, feature '$feature' not valid"
 			return 8
 		fi
 	done
 	# remove extraneous commas
-	caddy_features=$(sed 's/ /,/g' <<<${featurelist[@]})
+	features=$(sed 's/ /,/g' <<<${featurelist[@]})
 
 	# determine fresh install location
-	if [[ -z $caddy_path ]]; then
-		install_path="/usr/local/bin"
-
+	if [[ -z $caddy_cmd ]]
+	then
+		local install_path="/usr/local/bin"
 		# Termux on Android has $PREFIX set which already ends with /usr
-		if [[ -n "$ANDROID_ROOT" && -n "$PREFIX" ]]; then
-			install_path="$PREFIX/bin"
-		fi
-
+		[[ -n "$ANDROID_ROOT" && -n "$PREFIX" ]] && install_path="$PREFIX/bin"
 		# Fall back to /usr/bin if necessary
-		if [[ ! -d $install_path ]]; then
-			install_path="/usr/bin"
-		fi
-		caddy_path="$install_path/$caddy_bin"
+		[[ ! -d $install_path ]] && install_path="/usr/bin"
+		caddy_cmd="$install_path/$caddy_bin"
 	fi
 
 	########################
 	# Download and extract #
 	########################
 
-	echo "Downloading Caddy for $caddy_os/$caddy_arch..."
+	echo "Downloading Caddy for $caddy_os/$caddy_arch"
+	local tmp
 	[[ -n "$ANDROID_ROOT" && -n "$PREFIX" ]] && tmp=$PREFIX/tmp || tmp=/tmp
-	caddy_dl="$tmp/caddy_${caddy_os}_$caddy_arch${caddy_arm}_custom$caddy_dl_ext"
-	caddy_url="https://caddyserver.com/download/build?os=$caddy_os&arch=$caddy_arch&arm=$caddy_arm&features=$caddy_features"
-	echo "$caddy_url"
+	local caddy_dl="$tmp/caddy_${caddy_os}_$caddy_arch${caddy_arm}_custom$caddy_dl_ext"
+	local caddy_url="https://caddyserver.com/download/build?os=$caddy_os&arch=$caddy_arch&arm=$caddy_arm&features=$features"
+	echo "URL: $caddy_url"
 	$dl_cmd "$caddy_url" >"$caddy_dl"
 
-	echo "Extracting..."
+	echo "Extracting"
 	case "$caddy_dl" in
 		*.zip)    unzip -o "$caddy_dl" "$caddy_bin" -d "$tmp/" ;;
 		*.tar.gz) tar -xzf "$caddy_dl" -C "$tmp/" "$caddy_bin" ;;
@@ -199,35 +195,39 @@ install_caddy()
 	chmod +x "$tmp/$caddy_bin"
 
 	# Not every platform has or needs sudo (see issue #40)
+	local sudo_cmd
 	((EUID)) && [[ -z "$ANDROID_ROOT" ]] && sudo_cmd="sudo"
 
 	# Back up existing caddy, if any
-	caddy_cur_ver="$("$caddy_path" -version 2>/dev/null | cut -d ' ' -f2)"
-	if [[ $caddy_cur_ver ]]; then
+	local caddy_version=$("$caddy_cmd" -version || true)
+	if [[ $caddy_version ]]
+	then
 		# caddy of some version is already installed
-		caddy_backup="${caddy_path}_$caddy_cur_ver"
-		echo "Backing up $caddy_path to $caddy_backup"
+		local caddy_backup="${caddy_cmd}_${caddy_version##* }"
+		echo "Backing up $caddy_cmd to $caddy_backup"
 		echo "(Password may be required.)"
-		$sudo_cmd cp -v --backup=numbered "$caddy_path" "$caddy_backup"
+		$sudo_cmd cp -v --backup=numbered "$caddy_cmd" "$caddy_backup"
 	fi
 
-	[[ $pid ]] && echo "Stopping caddy" && kill -INT $pid && true
-	echo "Putting caddy in $caddy_path (may require password)"
-	$sudo_cmd mv "$tmp/$caddy_bin" "$caddy_path"
-	if setcap_cmd=$(type -p setcap); then
-		$sudo_cmd "$setcap_cmd" cap_net_bind_service=+ep "$caddy_path"
+	((pid)) && echo "Stopping caddy" && kill -INT $pid && true
+	echo "Putting caddy in $caddy_cmd (may require password)"
+	$sudo_cmd mv "$tmp/$caddy_bin" "$caddy_cmd"
+	if local setcap_cmd=$(type -p setcap)
+	then
+		echo "Allowing lower port numbers through setcap"
+		$sudo_cmd "$setcap_cmd" cap_net_bind_service=+ep "$caddy_cmd"
 	fi
-	[[ $pid ]] && echo "Restarting caddy" && "$caddy_path"
+	((pid)) && echo "Restarting caddy" && "$caddy_cmd"
 	$sudo_cmd rm -- "$caddy_dl"
 
 	# check installation
-	echo "Version: $("$caddy_path" --version)"
+	echo "Version: $("$caddy_cmd" --version)"
 
 	echo "Successfully installed"
 	trap ERR
 	return 0
 }
 
-install_caddy "$@"
-return=$?
-((return)) && echo "Not completed, aborted at $return"
+getcaddy "$@"
+getcaddy_return=$?
+((getcaddy_return)) && echo "Not completed, aborted at $getcaddy_return"
